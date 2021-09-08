7f9cded2a61c7ab66bcdd12a67e22f6aa6ea4711
masato0210 <88062069+masato0210@users.noreply.github.com>
2021-09-09 07:55:46 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+// swiper
+const Myswiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  /*direction: 'vertical',*/
+  loop: true,
+  effect: "fade",
+  speed: 1000,
+  autoplay: {
+  deplay: 2000
+},
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+// navigation
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
